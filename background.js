c906e9b604bae9b5a95e42b9ed99757e9f041f41
Z Yin <Z.Yin.MD@gmail.com>
2022-03-10 08:17:04 -0500

    link to help page

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === 'install') {
-    chrome.tabs.create({ url: 'https://foo.com' });
+    chrome.tabs.create({
+      url: 'https://zyinmd.github.io/hacker-news-keyboard-navigation/help.html',
+    });
   }
 });
 
 chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: 'https://foo.com' });
+  chrome.tabs.create({ url: 'https://zyinmd.github.io/hacker-news-keyboard-navigation/help.html' });
 });
