7d697dabd1f92192fa252793c4036b5808258fdb
Z Yin <Z.Yin.MD@gmail.com>
2022-03-09 23:43:37 -0500

    add keyboard listeners

--- scriptTag.js
+++ scriptTag.js
@@ -1,1 +1,31 @@
-console.log('hello world');
+document.addEventListener('keydown', (e) => {
+  if (e.isComposing) return; // if current keypress is in an input method, don't mess with it.
+  switch (e.code) {
+    case 'ArrowDown':
+      e.preventDefault();
+      console.log('down');
+      break;
+    case 'ArrowUp':
+      e.preventDefault();
+      console.log('up');
+      break;
+    case 'ArrowLeft':
+      e.preventDefault();
+      console.log('left');
+      break;
+    case 'ArrowRight':
+      e.preventDefault();
+      console.log('right');
+      break;
+    case 'Enter':
+      e.preventDefault();
+      console.log('enter');
+      break;
+    case 'Space':
+      e.preventDefault();
+      console.log('space');
+      break;
+    default:
+      break;
+  }
+});
