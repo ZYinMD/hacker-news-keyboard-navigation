41aee08900a454a68ef21f87c9aef11833733892
Z Yin <Z.Yin.MD@gmail.com>
2022-03-10 09:54:38 -0500

    improve version script

--- version.sh
+++ version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+# update version in manifest
 node version.cjs
 
 # make new zip
 rm -f dist.zip
-cd src
+cd src # cd in and cd out to avoid including the "src" folder in the zip
 zip -r ../dist *
 cd ..
 
+# changes created by version.sh will need to be added before the automatic version commit
 git add -A
