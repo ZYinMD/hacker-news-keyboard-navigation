9c4c61669822c00a6c1207a5a97d1daf96e3456b
Z Yin <Z.Yin.MD@gmail.com>
2022-03-12 03:38:53 -0500

    refactor: rename only

--- src/keypressHandlers/space.js
+++ src/keypressHandlers/space.js
@@ -1,8 +1,8 @@
 import { currentFocus } from './utils.js';
 
 export function onSpace() {
-  const commentsLink = document.querySelector(`a[href="item?id=${currentFocus.id}"]`);
-  commentsLink.target = '_blank';
-  commentsLink.click();
-  commentsLink.target = '_self';
+  const theCommentsLink = document.querySelector(`a[href="item?id=${currentFocus.id}"]`);
+  theCommentsLink.target = '_blank';
+  theCommentsLink.click();
+  theCommentsLink.target = '_self';
 }
