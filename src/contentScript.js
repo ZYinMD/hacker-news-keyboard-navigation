e1e52609573d0a419a790486b0379a1094dae51f
Z Yin <Z.Yin.MD@gmail.com>
2022-03-15 11:19:53 -0400

    disable the extension on some hn pages

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,10 @@
 /* inject our script tag unless currently in a page with comments */
-if (!location.search.includes('id=')) {
+if (
+  !location.search.includes('id=') &&
+  !location.pathname.includes('comments') && // this includes /newcomments and /bestcomments /noobcomments
+  !location.pathname.includes('submit') &&
+  !location.pathname.includes('leaders')
+) {
   const scriptTag = document.createElement('script');
   scriptTag.src = chrome.runtime.getURL('scriptTag.js');
   scriptTag.type = 'module';
