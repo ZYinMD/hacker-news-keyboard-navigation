6bca0a55faec7c2a7c71e8a19ab86637db7e28a8
Z Yin <Z.Yin.MD@gmail.com>
2022-03-17 07:24:27 -0400

    avoid multiple fires of same tab

--- src/sw.js
+++ src/sw.js
@@ -10,10 +10,13 @@ chrome.action.onClicked.addListener(() => {
   chrome.tabs.create({ url: 'https://zyinmd.github.io/hacker-news-keyboard-navigation/help.html' });
 });
 
+let lastOpened = '';
 chrome.runtime.onMessage.addListener((message, sender) => {
   if (sender.id !== chrome.runtime.id) return;
   const { type, url } = message;
   if (type === 'open_in_background_tab') {
+    if (url === lastOpened) return; // avoid multiple fires, which happens when 2+ tabs of hacker news is open, because bc will send message to all contentScripts.
     chrome.tabs.create({ url, active: false });
+    lastOpened = url;
   }
 });
